ba1f4091e5f2ad4a2cc6bff0995357a4f9629396
Josh Mills <millsaj@pm.me>
2025-06-17 16:48:46 -0600

    Fade page down button out when scrolling down anyway

--- app/_components/home/PageDownButton.tsx
+++ app/_components/home/PageDownButton.tsx
@@ -1,8 +1,21 @@
 'use client';
 
 import { ChevronDownIcon } from '@heroicons/react/24/outline';
+import { useState, useEffect } from 'react';
 
 export const PageDownButton = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > window.innerHeight / 2);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   const handleScroll = () => {
     window.scrollTo({
       top: window.innerHeight,
@@ -11,10 +24,10 @@ export const PageDownButton = () => {
   };
 
   return (
-    <div className="invisible sm:visible absolute bottom-8 left-1/2 transform -translate-x-1/2 z-100">
+    <div className={`invisible sm:visible transition-opacity duration-300 absolute bottom-8 left-1/2 transform -translate-x-1/2 z-100 ${isScrolled ? 'opacity-0' : 'opacity-100'}`}>
       <button
         onClick={handleScroll}
-        className="animate-bounce bg-primary hover:bg-primary rounded-full p-4"
+        className="animate-bounce bg-primary hover:bg-primary hover:cursor-pointer rounded-full p-4"
       >
         <ChevronDownIcon className="h-6 w-6 text-white" />
       </button>
